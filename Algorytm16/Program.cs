a7a2f3351bd81cf130b0242701439338107ebc83
tymonomyt <tymoteusz1138@gmail.com>
2022-10-24 20:26:40 +0200

    Added Algorithm#16

---
+++ Algorytm16/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Algorytm16
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj słowo do sprawdzenia!");
+            string input = new string(Console.ReadLine().ToLower().Where(x => char.IsLetter(x)).ToArray());
+            if (CheckPalindrome(input))
+            {
+                Console.WriteLine(input);
+            }
+            else
+            {
+                List<string> palindromes = new List<string>();
+                var newInputs = AddLetters(input);
+                foreach (var newInput in newInputs)
+                {
+                    if (CheckPalindrome(newInput))
+                    {
+                        palindromes.Add(newInput);
+                    }
+                }
+
+                while (palindromes.Count == 0)
+                {
+                    newInputs = GenerateLongerInputs(newInputs);
+                    foreach (var newInput in newInputs)
+                    {
+                        if (CheckPalindrome(newInput))
+                        {
+                            palindromes.Add(newInput);
+                        }
+                    }
+                }
+                var result = palindromes.OrderBy(x => x).First();
+                Console.WriteLine(result);
+            }
+        }
+
+        public static bool CheckPalindrome(string input)
+        {
+            bool palindrome = true;
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - i - 1])
+                {
+                    palindrome = false;
+                }
+            }
+
+            return palindrome;
+        }
+
+        public static List<string> AddLetters(string input)
+        {
+            List<string> newInputs = new();
+            for (int i = 0; i <= input.Length; i++)
+            {
+                for (int j = 97; j <= 122; j++)
+                {
+                    string newInput = input[..i] + (char)j + input[i..];
+                    newInputs.Add(newInput);
+                }
+            }
+
+            return newInputs;
+        }
+
+        public static List<string> GenerateLongerInputs(List<string> inputs)
+        {
+            List<string> longerInputs = new();
+            foreach (var newInput in inputs)
+            {
+                longerInputs.AddRange(AddLetters(newInput));
+            }
+            return longerInputs;
+        }
+    }
+}
---
+++ Algorytm16/README.md
@@ -0,0 +1,13 @@
+[algorytm#16]
+
+Wybaczcie opóźnienie, ale przygotowania do webinaru zjadły cały mój czas.
+
+Mając podany wyraz, znajdź palindrom, który zostanie utworzony poprzez dodanie jak najmniejszej liczby znaków jak to możliwe w dowolne miejsce w tym słowie. String po dodaniu liter nie musi tworzyć poprawnego słowa.
+Jeśli można stworzyć więcej niż jeden palindrom używając tej samej najmniejszej liczby znaków, zwróć ten który jest pierwszy pod względem alfabetycznym.
+
+Na przykład, podając słowo "rota", program powinien zwrócić string "atorota, ponieważ tworzymy palindrom dodając jedynie 3 litery. 
+Można w tym przykładzie stworzyć inne palindromy dodając 3 litery np. "rotator", ale "atorota" jest wcześniej pod względem alfabetycznym.
+
+Innym przykładem jest słowo "google", dla którego program powinien zwrócić string "elgoogle".
+
+Miłej zabawy!
