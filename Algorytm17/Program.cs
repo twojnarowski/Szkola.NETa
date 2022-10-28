4504d80f98e0ceb7ba29853db8caa7875c6fab7c
tymonomyt <tymoteusz1138@gmail.com>
2022-10-28 12:10:42 +0200

    Added algorithm 17

---
+++ Algorytm17/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = new int[] { 5, 100, 4, 200, 1, 3, 2, 99, 98, 101, 102, 103, 104, 105, 106, 107, 109, 110, 108, 110, 111, 112, 113, -1, -2, 0, -3, -4, 6, 7 };
+var arraySorted = array.OrderBy(x => x).ToArray();
+int globalMax = 0;
+int[] globalMaxArray = new int[0];
+int localMax = 1;
+int newStart = 0;
+for (int current = 1; current < arraySorted.Length; current++)
+{
+    if (arraySorted[current] - 1 == arraySorted[current - 1])
+    {
+        localMax++;
+    }
+    else
+    {
+        if (localMax > globalMax)
+        {
+            globalMax = localMax;
+            int end = newStart + localMax;
+            globalMaxArray = arraySorted[newStart..end];
+        }
+        localMax = 1;
+        newStart = current;
+    }
+}
+
+Console.WriteLine($"Najdłuższy ciąg ma długość {globalMax} i jest to {string.Join(", ", globalMaxArray)}");
---
+++ Algorytm17/README.md
@@ -0,0 +1,19 @@
+[algorytm#17]
+Dzisiaj zadanie rekrutacyjne z Microsoftu 🙂
+
+Mając podaną nieposortowaną listę intów, znajdź najdłuższy ciąg kolejnych liczb.
+
+PRZYKŁAD
+Dla podanej listy
+[100, 4, 200, 1, 3, 2]
+
+Najdłuższym ciągiem kolejnych liczb to sekwencja
+[1, 2, 3, 4]
+
+W związku z tym aplikacja powinna zwrócić
+Najdłuższy ciąg ma długość 4 i jest to [1, 2, 3, 4]
+
+
+UWAGA
+Twój algorytm powinien mieć złożoność 
+O(n)
