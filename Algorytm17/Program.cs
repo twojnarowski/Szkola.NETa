f4b64e4f84b8c83af2c163db8c667e28d3590184
tymonomyt <tymoteusz1138@gmail.com>
2022-10-28 12:17:15 +0200

    Fixed algorithm to handle longest possible subsequence, not longest actual subsequence of all sorted ints

--- Algorytm17/Program.cs
+++ Algorytm17/Program.cs
@@ -1,5 +1,5 @@
-﻿int[] array = new int[] { 5, 100, 4, 200, 1, 3, 2, 99, 98, 101, 102, 103, 104, 105, 106, 107, 109, 110, 108, 110, 111, 112, 113, -1, -2, 0, -3, -4, 6, 7 };
-var arraySorted = array.OrderBy(x => x).ToArray();
+﻿int[] array = new int[] { 5, 100, 4, 200, 1, 3, 3, 2, 99, 98, 101, 102, 103, 104, 105, 106, 107, 109, 110, 108, 108, 110, 111, 112, 113, -1, -2, 0, -3, -4, 6, 7 };
+var arraySorted = array.Distinct().OrderBy(x => x).ToArray();
 int globalMax = 0;
 int[] globalMaxArray = new int[0];
 int localMax = 1;
