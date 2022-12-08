2765d10d1318580cabc395f22afe0e67bd2c10f5
Tymonello <tymoteusz1138@gmail.com>
2022-12-08 21:55:55 +0100

    Update Program.cs

--- Zostań programistą ASP.NET/Tydzień2/TypyDanychZadania/Program.cs
+++ Zostań programistą ASP.NET/Tydzień2/TypyDanychZadania/Program.cs
@@ -1,4 +1,12 @@
-﻿//1
+// 1. Stwórz program, w którym zadeklarujesz zmienne dotyczące danych pracownika firmy. Dane
+// które chcesz przetrzymywać to:
+// a. Imię,
+// b. Nazwisko
+// c. Wiek
+// d. Płeć (‘m’ albo ‘k’)
+// e. PESEL
+// f. Numer pracownika (np. 2509324094)
+// Zadeklaruj zmienne odpowiednich typów, które mogą przetrzymywać te informacje
 
 string firstName;
 string lastName;
@@ -7,14 +15,16 @@ char sex;
 string pesel;
 int workerNumber;
 
-//2
+// 2. Napisz program, w którym stworzysz 3 zmienne z jedną literą, a następnie wypiszesz je w
+// odwrotnej kolejności niż zostały zadeklarowane
 
 char char1 = 'a';
 char char2 = 'b';
 char char3 = 'c';
 Console.WriteLine($"{char3} {char2} {char1}");
 
-//3
+// 3. Napisz program, który na podstawie podanej szerokości i długości prostokąta wyliczy długość
+// przekątnej. (Aby, obliczyć kwadrat liczby użyj metody Math.Pow())
 
 double width;
 double height;
@@ -24,13 +34,21 @@ if (double.TryParse(Console.ReadLine(), out width) && double.TryParse(Console.Re
     double diagonal = Math.Sqrt(Math.Pow(width, 2) + Math.Pow(height, 2));
 }
 
-//4
+// 4. Napisz program w którym stworzysz 2 zmienne liczbowe, oraz jedną tekstową, a następnie
+// przypiszesz im następujące wartości:
+// a. 10
+// b. Szkoła Dotneta
+// c. 12,5
+// Pamiętaj o użyciu poprawnych typów danych.
 
 int a = 10;
 string b = "Szkoła Dotneta";
 double c = 12.5;
 
-//5
+// 5. Napisz program w którym poprosisz użytkownika o jego dane personalne tj. Imię, nazwisko,
+// numer telefonu, adres email, wzrost, waga (np. 85,7), itp (postaraj się wymyślić jak najwięcej)
+// i spróbuj przekonwertować odpowiedź do odpowiedniego typu danych używając metody:
+// typDanych.Parse(odpowiedźOdUżytkownika)
 
 Console.WriteLine("Podaj swoje imie:");
 firstName = Console.ReadLine();
