fe483eba74d3c26dc53a64faa345c7bf08099f33
twojnarowski <tym17@24x7.pl>
2022-12-26 20:33:48 +0100

    Added algorithm 20

---
+++ Discord/Algorytmy/Algorytm20/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> sortedNumbers = new() { 1, 2, 3, 4, 5 };
+List<int> mixedNumbers = new();
+int len = sortedNumbers.Count;
+for (int i = 0; i < len; i++)
+{
+    int index;
+    if (i % 2 == 0)
+    {
+        index = 0;
+    }
+    else
+    {
+        index = sortedNumbers.Count - 1;
+    }
+
+    mixedNumbers.Add(sortedNumbers[index]);
+    sortedNumbers.RemoveAt(index);
+}
+
+string result = string.Join(", ", mixedNumbers.ToArray());
+Console.WriteLine(result);
