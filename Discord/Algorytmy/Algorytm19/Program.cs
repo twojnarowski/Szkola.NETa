1654debd162259be113e82bfd1673320b54e802b
twojnarowski <tym17@24x7.pl>
2022-12-26 20:19:53 +0100

    Added Algorytm19

---
+++ Discord/Algorytmy/Algorytm19/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Algorytm19;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj tekst do sprawdzenia!");
+        string inputSentence = Console.ReadLine().ToUpper();
+        Console.WriteLine("Podaj wzór do znalezienia!");
+        string inputPhrase = Console.ReadLine().ToUpper();
+        if (!inputSentence.Contains(inputPhrase))
+        {
+            Console.WriteLine("Nie znaleziono...");
+            return;
+        }
+
+        List<int> indices = new();
+        while (inputSentence.Contains(inputPhrase))
+        {
+            int index = inputSentence.IndexOf(inputPhrase);
+            indices.Add(index + indices.Count);
+            inputSentence = inputSentence.Remove(index, 1);
+        }
+
+        Console.WriteLine($"[{string.Join(", ", indices.ToArray())}]");
+    }
+}
